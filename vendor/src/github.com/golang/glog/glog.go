55c8ed6355dc32cceae9dce9e1a3043baeae2d67
jenson <jenson.zuo@gmail.com>
2017-09-03 07:44:22 +0800

    change default value for glog.logtostderr=true

--- vendor/src/github.com/golang/glog/glog.go
+++ vendor/src/github.com/golang/glog/glog.go
@@ -396,7 +396,7 @@ type flushSyncWriter interface {
 }
 
 func init() {
-	flag.BoolVar(&logging.toStderr, "logtostderr", false, "log to standard error instead of files")
+	flag.BoolVar(&logging.toStderr, "logtostderr", true, "log to standard error instead of files")
 	flag.BoolVar(&logging.alsoToStderr, "alsologtostderr", false, "log to standard error as well as files")
 	flag.Var(&logging.verbosity, "v", "log level for V logs")
 	flag.Var(&logging.stderrThreshold, "stderrthreshold", "logs at or above this threshold go to stderr")
