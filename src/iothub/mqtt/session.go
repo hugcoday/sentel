999d885e0a9c4d61f09b686b9816718471312f78
ssddn <ssddn@163.com>
2017-09-08 16:18:26 +0800

    change handlePublish logic

--- src/iothub/mqtt/session.go
+++ src/iothub/mqtt/session.go
@@ -76,7 +76,10 @@ type mqttSession struct {
 	sendPacketChannel chan *mqttPacket
 	sendMsgChannel    chan *mqttMessage
 	waitgroup         sync.WaitGroup
+
+	// resume field
 	msgs              []*mqttMessage
+	storedMsgs        map[uint16]*mqttMessage
 }
 
 // newMqttSession create new session  for each client connection
@@ -112,6 +115,7 @@ func newMqttSession(m *mqtt, conn net.Conn, id string) (*mqttSession, error) {
 		authapi:           authapi,
 		sendMsgChannel:    make(chan *mqttMessage, msgqsize),
 		msgs:              make([]*mqttMessage, msgqsize),
+		storedMsgs:        make(map[uint16]*mqttMessage),
 	}
 
 	return s, nil
@@ -610,7 +614,6 @@ func (s *mqttSession) handlePublish() error {
 	var mid uint16
 	var err error
 	var payload []uint8
-	var stored bool
 
 	dup := (s.inpacket.command & 0x08) >> 3
 	qos := (s.inpacket.command & 0x06) >> 1
@@ -663,10 +666,10 @@ func (s *mqttSession) handlePublish() error {
 
 	// Check wether the message has been stored
 	if qos > 0 {
-		if found, err := s.storage.FindMessage(s.id, mid); err != nil {
-			return err
+		if _, ok := s.storedMsgs[mid]; !ok {
+			dup = 1
 		} else {
-			stored = found
+			dup = 0
 		}
 	}
 	msg := StorageMessage{
@@ -676,15 +679,6 @@ func (s *mqttSession) handlePublish() error {
 		Qos:       qos,
 		Retain:    (retain > 0),
 		Payload:   payload,
-	}
-
-	if !stored {
-		dup = 0
-		if err := s.storage.StoreMessage(s.id, msg); err != nil {
-			return err
-		}
-	} else {
-		dup = 1
 	}
 
 	switch qos {
