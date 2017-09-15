76c728851d2a84d30e6a399459551c15f0dfd970
jenson <jenson.zuo@gmail.com>
2017-09-15 09:26:45 +0800

    add rest api service

--- iothub/main.go
+++ iothub/main.go
@@ -62,6 +62,6 @@ func init() {
 	base.RegisterService("mqtt:tcp", mqtt.Configs, &mqtt.MqttFactory{})
 	base.RegisterService("mqtt:ssl", mqtt.Configs, &mqtt.MqttFactory{})
 	base.RegisterService("mqtt:ws", mqtt.Configs, &mqtt.MqttFactory{})
-	base.RegisterService("api", api.Configs, &api.ApiServiceFactory{})
+	base.RegisterService("api", api.Configs, &api.RpcApiServiceFactory{})
 	base.RegisterService("dashboard", dashboard.Configs, &dashboard.DashboardServiceFactory{})
 }
