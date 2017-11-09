eb91d394d8ffe533c518fc3a232f8a949976d28c
jenson <jenson.zuo@gmail.com>
2017-11-09 22:31:04 +0800

    add changes for iothub apiservice

--- iothub/apiservice.go
+++ iothub/apiservice.go
@@ -14,11 +14,14 @@ package iothub
 
 import (
 	"errors"
+	"net/http"
 	"sync"
+	"time"
 
 	mgo "gopkg.in/mgo.v2"
 
 	"github.com/cloustone/sentel/core"
+	"github.com/golang/glog"
 	"github.com/labstack/echo"
 )
 
@@ -117,19 +120,66 @@ func (s *ApiService) Stop() {
 
 // addTenant add a tenant to iothub
 func addTenant(ctx echo.Context) error {
-	//config := ctx.(*apiContext).Config
-	//id := ctx.Param("id")
-	return nil
+	glog.Infof("iothub: add tenant(%s)", ctx.Param("id"))
+
+	id := ctx.Param("id")
+	hub := getIothub()
+	if err := hub.addTenant(id); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, &response{Success: false, Message: err.Error()})
+	}
+
+	return ctx.JSON(http.StatusOK, &response{Success: true})
 }
 
 // deleteTenant delete a tenant from iothub
 func deleteTenant(ctx echo.Context) error {
-	return nil
+	glog.Infof("iothub: delete tenant(%s)", ctx.Param("id"))
+
+	id := ctx.Param("id")
+	hub := getIothub()
+	if err := hub.deleteTenant(id); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, &response{Success: false, Message: err.Error()})
+	}
+
+	return ctx.JSON(http.StatusOK, &response{Success: true})
+}
+
+type tenantResponse struct {
+	id           string            `json:"tenantId"`
+	createdAt    time.Time         `json:"createdAt"`
+	brokersCount int               `json:"brokersCount"`
+	brokers      map[string]string `json:"brokers"`
 }
 
 // getTenantInfo retrieve a tenant information from iothub
 func getTenantInfo(ctx echo.Context) error {
-	return nil
+	glog.Infof("iothub: get tenant(%s) info", ctx.Param("id"))
+
+	tid := ctx.Param("id")
+	hub := getIothub()
+	tenant := hub.getTenant(tid)
+	if tenant == nil {
+		return ctx.JSON(http.StatusInternalServerError,
+			&response{Success: false, Message: "Invalid tenant id"})
+	}
+
+	rsp := &tenantResponse{
+		id:           tid,
+		createdAt:    tenant.createdAt,
+		brokersCount: tenant.brokersCount,
+		brokers:      make(map[string]string),
+	}
+	for bid, broker := range tenant.brokers {
+		switch broker.status {
+		case BrokerStatusInvalid:
+			rsp.brokers[bid] = "invalid"
+		case BrokerStatusStarted:
+			rsp.brokers[bid] = "started"
+		case BrokerStatusStoped:
+			rsp.brokers[bid] = "stoped"
+		}
+	}
+	return ctx.JSON(http.StatusOK, &response{Success: true, Result: rsp})
 }
 
 // Broker
